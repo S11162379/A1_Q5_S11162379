4fff5041ab33a008746973e8a7c89d25c6093aaf
S11162379 <s11162379@student.usp.ac.fj>
2021-10-09 22:10:15 +1200

    Final version 1.0

--- src/test/java/SnakeTest.java
+++ src/test/java/SnakeTest.java
@@ -1,107 +1,122 @@
-import org.junit.Ignore;
-import org.junit.jupiter.api.*;
+    /*
+     * This file contains the test cases for the snake game in Q5
+     *
+     * Compiled by Niheet N. Prasad // S11162379
+     *
+     * More possible test cases (not implemented due to time constraints):
+     *
+     * 1. Check default location of snake
+     * 2. Check if food spawns
+     * 3. Check if food despawns when eaten
+     * 4. Check if snake gets longer when eating food
+     * 5. Coordinates change when the snake moves every frame
+     * 6. Check if game stops when snake hits itself
+     *
+     *
+     * */
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+    import org.junit.jupiter.api.Assertions;
+    import org.junit.jupiter.api.BeforeAll;
+    import org.junit.jupiter.api.DisplayName;
+    import org.junit.jupiter.api.Test;
 
-class SnakeTest {
+    import java.awt.event.KeyEvent;
 
-    Window instance = new Window();
-    KeyboardListener keyboardListener = new KeyboardListener();
+    class SnakeTest {
 
-    Tuple position = new Tuple(10, 10);
-    ThreadsController threadsController = new ThreadsController(position);
+        //Instantiate a window to allow for testing
+        Window instance = new Window();
+        KeyboardListener keyboardListener = new KeyboardListener();
 
-    KeyEvent up = new KeyEvent(instance, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_UP, 'W');
-    KeyEvent down = new KeyEvent(instance, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_DOWN, 'S');
-    KeyEvent left = new KeyEvent(instance, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_LEFT, 'A');
-    KeyEvent right = new KeyEvent(instance, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT, 'D');
+        //Defining keyEvents to simulate keyboard input by the user
+        KeyEvent up = new KeyEvent(instance, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_UP, 'W');
+        KeyEvent down = new KeyEvent(instance, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_DOWN, 'S');
+        KeyEvent left = new KeyEvent(instance, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_LEFT, 'A');
+        KeyEvent right = new KeyEvent(instance, KeyEvent.KEY_PRESSED, System.currentTimeMillis(), 0, KeyEvent.VK_RIGHT, 'D');
 
 
-    @BeforeAll
-    public static void setupAll() {
-        System.out.println("\nInitiating testing sequence...");
+        @BeforeAll
+        public static void setupAll() {
+            System.out.println("\nInitiating testing sequence...");
+        }
+
+        //Checking if an invalid value for the color parameter is handled (less than 0)
+        @Test
+        @DisplayName("Checking if col is less than 0")
+        public void ThrowRuntimeExceptionIfColIsLessThanZero() {
+
+            Assertions.assertThrows(RuntimeException.class, () -> {
+                DataOfSquare dataOfSquare = new DataOfSquare(-1);
+            });
+        }
+
+        //Checking if an invalid value for the color parameter is handled (more than 2)
+        @Test
+        @DisplayName("Checking if col is more than 2")
+        public void ThrowRuntimeExceptionIfColIsMoreThanTwo() {
+
+            Assertions.assertThrows(RuntimeException.class, () -> {
+                DataOfSquare dataOfSquare = new DataOfSquare(3);
+            });
+        }
+
+        //Checking for invalid movement, i.e trying to go the direction that is opposite to the snake's path
+        //Check if going backwards from North
+        @Test
+        @DisplayName("Check if heading backwards from North")
+        public void ThrowRuntimeExceptionIfHeadingBackwardsFromNorth() {
+
+            instance.getKeyListeners()[0].keyPressed(up);
+            keyboardListener.keyPressed(up);
+            instance.getKeyListeners()[0].keyPressed(down);
+            keyboardListener.keyPressed(down);
+
+            Assertions.assertEquals(3, ThreadsController.directionSnake);
+        }
+
+        //Checking for invalid movement, i.e trying to go the direction that is opposite to the snake's path
+        //Check if going backwards from South
+        @Test
+        @DisplayName("Check if heading backwards from South")
+        public void ThrowRuntimeExceptionIfHeadingBackwardsFromSouth() {
+
+            instance.getKeyListeners()[0].keyPressed(down);
+            keyboardListener.keyPressed(down);
+            instance.getKeyListeners()[0].keyPressed(up);
+            keyboardListener.keyPressed(up);
+
+            Assertions.assertEquals(4, ThreadsController.directionSnake);
+        }
+
+        //Checking for invalid movement, i.e trying to go the direction that is opposite to the snake's path
+        //Check if going backwards from East
+        @Test
+        @DisplayName("Check if heading backwards from East")
+        public void ThrowRuntimeExceptionIfHeadingBackwardsFromEast() {
+
+            instance.getKeyListeners()[0].keyPressed(right);
+            keyboardListener.keyPressed(right);
+            instance.getKeyListeners()[0].keyPressed(left);
+            keyboardListener.keyPressed(left);
+
+            Assertions.assertEquals(1, ThreadsController.directionSnake);
+        }
+
+        //Checking for invalid movement, i.e trying to go the direction that is opposite to the snake's path
+        //Check if going backwards from West
+        @Test
+        @DisplayName("Check if heading backwards from West")
+        public void ThrowRuntimeExceptionIfHeadingBackwardsFromWest() {
+
+            //Extra keypress to go downward as the default direction of the snake is towards East,
+            //thus the test always fails since directionSnake is always '1' and does not change.
+            instance.getKeyListeners()[0].keyPressed(down);
+            keyboardListener.keyPressed(down);
+            instance.getKeyListeners()[0].keyPressed(left);
+            keyboardListener.keyPressed(left);
+            instance.getKeyListeners()[0].keyPressed(right);
+            keyboardListener.keyPressed(right);
+
+            Assertions.assertEquals(2, ThreadsController.directionSnake);
+        }
     }
-
-    //Checking if an invalid value for the color parameter is handled
-    @Test
-    @DisplayName("Checking if col is less than 0")
-    public void ThrowRuntimeExceptionIfColIsLessThanZero() {
-
-        Assertions.assertThrows(RuntimeException.class, () -> {
-            DataOfSquare dataOfSquare = new DataOfSquare(-1);
-        });
-    }
-
-    @Test
-    @DisplayName("Checking if col is more than 2")
-    public void ThrowRuntimeExceptionIfColIsMoreThanTwo() {
-
-        Assertions.assertThrows(RuntimeException.class, () -> {
-            DataOfSquare dataOfSquare = new DataOfSquare(3);
-        });
-    }
-
-    //Checking for invalid movement, i.e trying to go the direction that is opposite to the snake's path
-    @Test
-    @DisplayName("Check if heading backwards from North")
-    public void ThrowRuntimeExceptionIfHeadingBackwardsFromNorth() {
-
-        instance.getKeyListeners()[0].keyPressed(up);
-        keyboardListener.keyPressed(up);
-        instance.getKeyListeners()[0].keyPressed(down);
-        keyboardListener.keyPressed(down);
-
-        Assertions.assertEquals(3, ThreadsController.directionSnake);
-    }
-
-    @Test
-    @DisplayName("Check if heading backwards from South")
-    public void ThrowRuntimeExceptionIfHeadingBackwardsFromSouth() {
-
-        instance.getKeyListeners()[0].keyPressed(down);
-        keyboardListener.keyPressed(down);
-        instance.getKeyListeners()[0].keyPressed(up);
-        keyboardListener.keyPressed(up);
-
-        Assertions.assertEquals(4, ThreadsController.directionSnake);
-    }
-
-    @Test
-    @DisplayName("Check if heading backwards from East")
-    public void ThrowRuntimeExceptionIfHeadingBackwardsFromEast() {
-
-        instance.getKeyListeners()[0].keyPressed(right);
-        keyboardListener.keyPressed(right);
-        instance.getKeyListeners()[0].keyPressed(left);
-        keyboardListener.keyPressed(left);
-
-        Assertions.assertEquals(1, ThreadsController.directionSnake);
-    }
-
-    @Test
-    @DisplayName("Check if heading backwards from West")
-    public void ThrowRuntimeExceptionIfHeadingBackwardsFromWest() {
-
-        //Extra keypress to go downward as the default direction of the snake is towards East,
-        //thus the test always fails since directionSnake is always '1' and does not change.
-        instance.getKeyListeners()[0].keyPressed(down);
-        keyboardListener.keyPressed(down);
-        instance.getKeyListeners()[0].keyPressed(left);
-        keyboardListener.keyPressed(left);
-        instance.getKeyListeners()[0].keyPressed(right);
-        keyboardListener.keyPressed(right);
-
-        Assertions.assertEquals(2, ThreadsController.directionSnake);
-    }
-
-
-    @Test
-    @DisplayName("Check if pauser throws exception when thread is interrupted")
-    public void ThrowRuntimeExceptionPauserThreadInterrupted() {
-
-        Assertions.assertThrows(InterruptedException.class, () -> {
-            threadsController.moveInterne(4);
-        });
-    }
-}
